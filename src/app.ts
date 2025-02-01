0105b52ae8bb0f8808e72c121e8d82181e5918b3
sajinshr3stha <shresthasajin59@gmail.com>
2025-02-01 19:20:53 +0545

    feat: swagger doc support

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,38 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJSDoc from 'swagger-jsdoc'
 import globalErrorHandler from './middlewares/globalErrorHandler'
 import userRouter from './user/userRouter'
 
 const app = express()
+
+// Swagger configuration
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.1.0',
+    info: {
+      title: 'E-Governance API',
+      version: '1.0.0',
+      description: 'API documentation for the E-Governance backend',
+    },
+  },
+  apis: ['./src/user/userRouter.ts'],
+}
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions)
+// Serve Swagger docs at `/api-docs`
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
 app.use(express.json())
 
-// routes
+// Routes
 app.get('/', (req, res, next) => {
   res.json({ message: 'Welcome to egovernance - backend' })
 })
 
 app.use('/api/users', userRouter)
 
-// global error-handler middleware
+// Global error-handler middleware
 app.use(globalErrorHandler)
 
 export default app
--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -3,7 +3,60 @@ import { createUser, loginUser } from './userController'
 
 const userRouter = express.Router()
 
+/**
+ * @swagger
+ * /api/users/register:
+ *   post:
+ *     tags:
+ *       - Users
+ *     summary: Create a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                  type: string
+ *                  example: 'testuser'
+ *               email:
+ *                 type: string
+ *                 example: 'john.doe@example.com'
+ *               password:
+ *                 type: string
+ *                 example: 'password@123!'
+ *             required:
+ *               - username
+ *               - email
+ *               - password
+ */
 userRouter.post('/register', createUser)
+
+/**
+ * @swagger
+ * /api/users/login:
+ *   post:
+ *     tags:
+ *       - Users
+ *     summary: Login an existing user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: 'john.doe@example.com'
+ *               password:
+ *                 type: string
+ *                 example: 'password@123!'
+ *             required:
+ *               - email
+ *               - password
+ */
 userRouter.post('/login', loginUser)
 
 export default userRouter
