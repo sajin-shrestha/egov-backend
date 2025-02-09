4345031467cfd433cb5e9dfa831695b4ed5a3f30
sajinshr3stha <shresthasajin59@gmail.com>
2025-02-09 19:42:28 +0545

    fix

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -6,10 +6,10 @@ const createFilter = (
 ): Record<string, { $regex: string; $options: string }> => {
   return Object.fromEntries(
     Object.entries(req.query)
-      .filter(([key]) => allowedFields.includes(key))
+      .filter(([key]) => allowedFields.includes(key)) // Ensure only allowed fields are included
       .map(([key, value]) => [
         key.toLowerCase(),
-        { $regex: String(value), $options: 'i' },
+        { $regex: String(value), $options: 'i' }, // Ensure proper conversion to string and case-insensitive matching
       ]),
   )
 }
