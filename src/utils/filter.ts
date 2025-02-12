ece0f8ae7894e7a6813d1f15f6a95b967c67051e
sajinshr3stha <shresthasajin59@gmail.com>
2025-02-12 17:36:58 +0545

    use populate in complainController to show user data

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,21 +1,3 @@
-// import { Request } from 'express'
-
-// const createFilter = (
-//   req: Request,
-//   allowedFields: string[],
-// ): Record<string, { $regex: string; $options: string }> => {
-//   return Object.fromEntries(
-//     Object.entries(req.query)
-//       .filter(([key]) => allowedFields.includes(key)) // Ensure only allowed fields are included
-//       .map(([key, value]) => [
-//         key.toLowerCase(),
-//         { $regex: String(value), $options: 'i' }, // Ensure proper conversion to string and case-insensitive matching
-//       ]),
-//   )
-// }
-
-// export default createFilter
-
 import { Request } from 'express'
 
 const createFilter = (
